67b2e91c42fbb75d9569c57694d64661dcf1b51a
mayagos <53911158+mayagos@users.noreply.github.com>
2023-08-15 22:10:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     const fileListContainer = document.querySelector('.file-list');
 
-    fetch('Saved Pictures/') // Fetch the list of files from the directory
-        .then(response => response.text())
-        .then(data => {
-            const filenames = data.split('\n').filter(filename => filename && filename !== 'desktop.ini');
-            filenames.forEach(filename => {
-                const downloadButton = document.createElement('a');
-                downloadButton.href = `Saved Pictures/${filename}`; // Use relative path
-                downloadButton.download = filename;
-                downloadButton.textContent = `Download ${filename}`;
-                downloadButton.classList.add('download-button');
-                fileListContainer.appendChild(downloadButton);
-            });
-        })
-        .catch(error => console.error('Error fetching file list:', error));
+    // Manually create an array of filenames
+    const imageFilenames = [
+        'cat.jpg', 
+        'copy_cat.jpg', 
+        'IMG_20220617_115827_resized_20230814_025220159.jpg', 
+        'IMG_20220617_122035_resized_20230814_025219160.jpg', 
+        'IMG_20220617_125628_resized_20230814_025219878.jpg', 
+        'IMG_20220617_125811_resized_20230814_025220445.jpg', 
+        'IMG_20220617_130526_resized_20230814_025218862.jpg', 
+        'IMG_20220720_122717_resized_20230814_025219470.jpg', 
+        'IMG_20220720_122757_resized_20230814_025218561.jpg', 
+        'IMG_20220720_122810_resized_20230814_025218241.jpg'
+    ];
+
+    imageFilenames.forEach(filename => {
+        const downloadButton = document.createElement('a');
+        downloadButton.href = `Saved Pictures/${filename}`; // Use relative path
+        downloadButton.download = filename;
+        downloadButton.textContent = `Download ${filename}`;
+        downloadButton.classList.add('download-button');
+        fileListContainer.appendChild(downloadButton);
+    });
 });
